48b20bd3e17043afaa9fa948626a46aa0e0714d4
Gursharan <gursharan1173.becse24@chitkara.edu.in>
2025-04-17 20:26:48 +0530

    Just started working

---
+++ output/script.js
@@ -0,0 +1,14 @@
+document.getElementById('SignupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (password.length < 8) {
+        errorMessage.textContent = 'Password must be at least 8 characters long.';
+    } else {
+        errorMessage.textContent = '';
+       
+        alert('Login successful!');
+    }
+});
